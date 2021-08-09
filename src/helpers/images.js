4313253ccc7fe0286bda921c7b3ab5dbbc2f4ddc
Matthew McKney <mrmckney@gmail.com>
2021-08-09 16:07:40 -0400

    added cropped image

--- src/helpers/images.js
+++ src/helpers/images.js
@@ -7,3 +7,38 @@ export function readFile(file) {
         reader.readAsDataURL(file)
     })
 }
+
+const createImage = url => {
+    return new Promise((resolve, reject) => {
+        const image = new Image()
+        image.addEventListener('load', () => resolve(image))
+        image.addEventListener('error', () => reject(error))
+        image.setAttribute('crossOrigin', 'anonymous')
+        image.src = url
+    })
+}
+
+export async function saveCroppedImage(fileName, imageSrc, croppedAreaPixels) {
+    const image = await createImage(imageSrc)
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+    const maxSize = Math.max(image.width, image.height)
+    canvas.width = maxSize
+    canvas.height = maxSize
+    ctx.drawImage(image, maxSize /2 - image.width /2 , maxSize /2 - image.height /2)
+    const data = ctx.getImageData(0, 0, maxSize, maxSize)
+    canvas.width = croppedAreaPixels.width
+    canvas.height = croppedAreaPixels.height
+    ctx.putImageData(data, 
+        Math.round(0 - maxSize /2 + image.width /2 - croppedAreaPixels.x ),
+        Math.round(0 - maxSize /2 + image.height /2 - croppedAreaPixels.y )
+    )
+    const url = canvas.toDataURL('image/jpg', 0.8)
+    const base64data = url.replace(/^data:image\/png;base64,/, '')
+    const newFileName = fileName + '-cropped.png'
+    fs.writeFile(newFileName, base64data, 'base64', err => {
+        if(err) {
+            console.error(err)
+        }
+    })
+}
